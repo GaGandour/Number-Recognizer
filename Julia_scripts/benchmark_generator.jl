963c18612181f4d515cfcf15cdf7d2da70432bc3
GaGandour <64374553+GaGandour@users.noreply.github.com>
2021-12-05 12:02:38 -0300

    grafico de barras

--- Julia_scripts/benchmark_generator.jl
+++ Julia_scripts/benchmark_generator.jl
@@ -10,19 +10,31 @@ using .TestExtractor
 include("image_classification.jl")
 using .ImageClassification
 
-test_Labels = get_testLabels()
-test_pixels = get_testPixels()
+using StatsPlots
 
-refs = construct_digit_references(1000, 10)
+function correct_guesses_byNorm()
+    test_Labels = get_testLabels()
+    test_pixels = get_testPixels()
 
-acertos = zeros(10)
-total = zeros(10)
+    refs = construct_digit_references(1000, 10)
 
-for i in eachindex(test_Labels)
-    total[test_Labels[i]+1] += 1
-    if classify_image(get_Image_byLabel_and_Matrix(test_pixels,i), 10, refs, 100.0)[1] == test_Labels[i]
-        acertos[test_Labels[i]+1] += 1
+    acertos = zeros(10)
+    total = zeros(10)
+
+    for i in eachindex(test_Labels)
+        total[test_Labels[i]+1] += 1
+        if classify_image(get_Image_byLabel_and_Matrix(test_pixels,i), 10, refs, 100.0)[1] == test_Labels[i]
+            acertos[test_Labels[i]+1] += 1
+        end
     end
+    return acertos./total
 end
 
-display(acertos./total)
+function generate_barPlot(byNorm::Vector{Float64}, byHypo::Vector{Float64})
+    ctg = repeat(["Sem teste de hipótese", "Com teste de hipótese"], inner = 10)
+    ticklabel = string.(collect('0':'9'))
+    groupedbar([byNorm 2*byHypo], bar_position = :dodge, bar_width=0.7, xticks=(1:10, ticklabel), group = ctg, xlabel = "Dígitos", ylabel = "Taxa de Acerto",
+    title = "Taxa de acerto por dígito por método", lw = 0, framestyle = :box, legend = :bottomleft)
+end
+
+generate_barPlot(correct_guesses_byNorm(),correct_guesses_byNorm())
